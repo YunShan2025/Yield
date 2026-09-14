e56214b2b374b1d7d11132d504aaa19ed279e121
YunShan2025 <1105274197@qq.com>
2026-09-14 17:15:32 +0800

    build(android): debug 构建开启 WebView DevTools
    供 adb forward 后用 CDP 做界面自动化验收（M1 交互验证即用此通道）。

--- src-tauri/gen/android/app/src/main/java/com/yunshan/youqiu/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/yunshan/youqiu/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.yunshan.youqiu
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    // Debug 构建开放 WebView DevTools，供 adb forward 后用 CDP 做界面自动化验收。
+    WebView.setWebContentsDebuggingEnabled(true)
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
   }
